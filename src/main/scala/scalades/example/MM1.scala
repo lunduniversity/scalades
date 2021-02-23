2dd1e7b1c10fdbdacc2ef69771ba14de188741c4
bjornregnell <bjorn.regnell@cs.lth.se>
2021-02-23 13:24:55 +0100

    refactor rename

--- src/main/scala/scalades/example/MM1.scala
+++ src/main/scala/scalades/example/MM1.scala
@@ -1,42 +1,47 @@
-package scalades
+package scalades.example
+import scalades.*
 
-class MM1(val lambda: Double, val mu: Double):
+//sbt "runMain scalades.example.runMM1 10.0 8.0 100.0"
+@main def runMM1(lambda: Double, mu: Double, endTime: Double): Unit =
+  MM1(lambda, mu).run(end=Time.Stamp(endTime))
+
+final class MM1(val lambda: Double, val mu: Double):
   def nextInterArrivalTime: Time.Duration = Time.Duration(RNG.negExp(lambda))
   def nextServiceTime: Time.Duration = Time.Duration(RNG.negExp(mu))
 
   val sim = new Simulation
 
-  enum Msg extends Message:
+  enum Sig extends Signal:
     case Generate, Job, JobDone
 
-  object generator extends Process(sim):
+  import Sig.*
+
+  object generator extends Process:
     def handle(e: Event) = e match
-      case Event(Msg.Generate, handler, from, now) =>
+      case Event(Generate, handler, from, now) =>
         println(s"Generator activated at $now.")
-        sim.add(Event(msg = Msg.Job, handler = server, from = this, time = now))
-        sim.add(Event(msg = Msg.Generate, handler = this, from = this, time = now + nextInterArrivalTime))
+        sim.add(Event(Job, handler = server, from = this, time = now))
+        sim.add(Event(Generate, handler = this, from = this, time = now + nextInterArrivalTime))
  
-  object server extends Process(sim):
+  object server extends Process:
     private var nbrOfJobsInQ = 0
     def n = nbrOfJobsInQ
 
     def handle(e: Event) = e match
-      case Event(Msg.Job, handler, from, now) =>
+      case Event(Job, handler, from, now) =>
         println(s"Server starts processing Job at $now. In queue: $nbrOfJobsInQ")
         if nbrOfJobsInQ == 0
-        then sim.add(Event(msg=Msg.JobDone, handler = this, from = this, time = now + nextServiceTime))
+        then sim.add(Event(JobDone, handler = this, from = this, time = now + nextServiceTime))
         nbrOfJobsInQ += 1
 
-      case Event(Msg.JobDone, handler, from, now) =>
+      case Event(JobDone, handler, from, now) =>
         nbrOfJobsInQ -= 1
         println(s"Server JobDone at $now. In queue: $nbrOfJobsInQ")
         if nbrOfJobsInQ > 0
-        then sim.add(Event(Msg.JobDone, handler = this, from = this, time = now + nextServiceTime))
+        then sim.add(Event(JobDone, handler = this, from = this, time = now + nextServiceTime))
 
-  def until(end: Time.Stamp) = 
-    sim.add(Event(msg = Msg.Generate, handler = generator, from = NoProcess, Time.zero))
+  def run(end: Time.Stamp) = 
+    sim.reset()
+    sim.add(Event(signal = Generate, handler = generator, from = null, Time.zero)) // null is ugly
     sim.simulate(until = end)
 
-object MM1:
-  @main def run(lambda: Double, mu: Double, endTime: Double): Unit =
-    new MM1(lambda, mu).until(end=Time.Stamp(endTime))
