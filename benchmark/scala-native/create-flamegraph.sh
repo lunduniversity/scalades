309dfd509d92fed1e38ba323af47c9a6f8f9ccde
bjornregnell <bjorn.regnell@cs.lth.se>
2021-03-15 15:35:08 +0100

    add gitgnore flamegraph intermediate output

---
+++ benchmark/scala-native/create-flamegraph.sh
@@ -0,0 +1,14 @@
+# assumes (1) you have perf installed on a linux system
+# sudo apt update && sudo apt install linux-tools-common
+
+# assumes (2) you have cloned https://github.com/brendangregg/FlameGraph
+# into this folder ~/git/hub/FlameGraph/
+# the svg flame graph is created in kernel.svg 
+
+# assumes (3) that you have done 
+# sbt run
+
+sudo perf record -F 99 -a -g ./target/scala-2.13/scala-native-out
+sudo perf script > out.perf
+~/git/hub/FlameGraph/stackcollapse-perf.pl out.perf > out.folded
+~/git/hub/FlameGraph/flamegraph.pl out.folded > kernel.svg
---
+++ benchmark/scala-native/run-time-memory.sh
@@ -0,0 +1,2 @@
+sbt "scala-native/nativeLink" # or just sbt run
+/usr/bin/time --verbose ./target/scala-2.13/scala-native-out
