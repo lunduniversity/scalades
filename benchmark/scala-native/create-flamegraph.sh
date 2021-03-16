a105909a4caa904ff80b52897411f644bed6a811
bjornregnell <bjorn.regnell@cs.lth.se>
2021-03-16 18:21:41 +0100

    change sample rate in flamegraphs to 1000 Hz

--- benchmark/scala-native/create-flamegraph.sh
+++ benchmark/scala-native/create-flamegraph.sh
@@ -8,7 +8,7 @@
 # assumes (3) that you have done 
 # sbt run
 
-sudo perf record -F 10000 -a -g ./target/scala-2.13/scala-native-out
+sudo perf record -F 1000 -a -g ./target/scala-2.13/scala-native-out
 sudo perf script > out.perf
 ~/git/hub/FlameGraph/stackcollapse-perf.pl out.perf > out.folded
 ~/git/hub/FlameGraph/flamegraph.pl out.folded > kernel.svg
