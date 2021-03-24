57a0b89ec9ce4932a1d73d545b813c85de6465ee
bjornregnell <bjorn.regnell@cs.lth.se>
2021-03-24 22:38:06 +0100

    add script to publish docs

---
+++ publish-docs.sh
@@ -0,0 +1,2 @@
+sbt doc
+scp -r target/scala-3.0.0-RC1/api $LUCATID@fileadmin.cs.lth.se:/Websites/Fileadmin/scalades/docs
--- update-docs.sh
+++
@@ -1,3 +0,0 @@
-rm -rf docs
-sbt doc
-cp -r target/scala-3.0.0-RC1/api/ docs
