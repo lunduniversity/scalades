57da249d5bd4ada7c850d7a84e9030ab73e1c4a3
bjornregnell <bjorn.regnell@cs.lth.se>
2021-04-20 21:38:52 +0200

    update doc script

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,3 +1,4 @@
+SCALAVERSION=scala-3.0.0-RC3
 sbt doc
 ssh $LUCATID@fileadmin.cs.lth.se "rm -rf scalades/docs"
-scp -r target/scala-3.0.0-RC1/api $LUCATID@fileadmin.cs.lth.se:/Websites/Fileadmin/scalades/docs
+scp -r target/$SCALAVERSION/api $LUCATID@fileadmin.cs.lth.se:/Websites/Fileadmin/scalades/docs
