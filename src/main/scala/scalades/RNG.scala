8efdf5bacf0ab371747b304475c67d5b7aa10016
bjornregnell <bjorn.regnell@cs.lth.se>
2021-04-20 21:14:25 +0200

    RNG refactor, rename, remove inline

--- src/main/scala/scalades/RNG.scala
+++ src/main/scala/scalades/RNG.scala
@@ -3,23 +3,20 @@ package scalades
 /** A thread-safe Random Number Generator (RNG).*/
 object RNG:
   /** Underlying thread-safe random number generator from Java JDK. */
-  val underlying = java.util.concurrent.ThreadLocalRandom.current
+  val underlying = //new java.util.Random()
+    java.util.concurrent.ThreadLocalRandom.current
 
   /** Random number with negative exponential distribution. */
-  inline def negExp(mean: Double): Double = 
-    math.log(underlying.nextDouble)/(-1 / mean) 
+  def negExp(mean: Double): Double = 
+    - math.log(underlying.nextDouble()) * mean 
     // same as math.log(1 - underlying.nextDouble)/(-1 / mean)
 
-  /** Random number with Poisson distribution. */
-  inline def poisson(intensity: Double): Double = 
-    RNG.negExp(1.0/intensity)
-
-  /** Random Time.Duration with Poisson distribution. */
-  inline def duration(intensity: Double): Time.Duration = 
-    Time.Duration(poisson(intensity))
+  /** Random Time.Duration with exponential distribution with a mean of 1/intensity. */
+  def duration(intensity: Double): Time.Duration = 
+    Time.Duration(negExp(1.0/intensity))
 
   /** Random integer number with rectangular distribution. */
-  inline def rect(from: Int, until: Int): Int = underlying.nextInt(from, until)
+  def rect(from: Int, until: Int): Int = underlying.nextInt(until - from) + until
 
   /** If the seed is set then the pseudo-random sequence is repeatable. */
-  inline def setSeed(seed: Long) = underlying.setSeed(seed)
+  def setSeed(seed: Long) = underlying.setSeed(seed)
